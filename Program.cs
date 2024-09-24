20431b2e6de02a3543d2c838450b2ae1c6aad253
Szabolcs <Szabolcs@SZABOLCS-PC>
2024-09-24 11:24:07 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OE_PMP_GYAK_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
